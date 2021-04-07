cb11f4ff10d9e5e053109f7bdf073713ddd873bd
Mariana Soares <mariana.dsantos@al.infnet.edu.br>
2021-04-06 21:17:17 -0300

    sobe projeto no remote

---
+++ app/src/main/java/com/example/assessment_design_android/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.assessment_design_android.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = " "
+    }
+    val text: LiveData<String> = _text
+}
